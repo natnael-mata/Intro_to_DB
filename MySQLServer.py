714caf70d3fbe795aed8111369d1b1c75c055fa4
natnael-mata <nati.sami.mata@gmail.com>
2025-10-25 18:16:22 +0300

    first commit

---
+++ MySQLServer.py
@@ -0,0 +1,23 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='admin',
+        password='Rainbow##321',
+        database='alx_book_store'
+    )
+
+    if connection.is_connected():
+        print("Connected to MySQL database")
+        print("Database 'alx_book_store' created successfully!")
+
+except Error as e:
+    print("Failed to establish a connection to the database")
+
+finally:
+    if 'connection' in locals() and connection.is_connected():
+        connection.close()
+        print("Connection closed")
+
--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,3 +1,4 @@
+tst
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 USE alx_book_store;
