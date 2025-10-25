c3a7401b7fda2ecc1a6d3b548a0d301e1ab1f2c4
natnael-mata <nati.sami.mata@gmail.com>
2025-10-25 19:03:57 +0300

    Second Push

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,13 @@
 use alx_book_store;
- SELECT
-   *
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
 FROM
-    information_schema.COLUMNS
+    INFORMATION_SCHEMA.COLUMNS
 WHERE
     TABLE_SCHEMA = 'alx_book_store'
     AND TABLE_NAME = 'Books';
